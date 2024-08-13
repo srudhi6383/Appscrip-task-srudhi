52bdf670941a1e3825a3cc6178dde144c92c7318
srudhi6383 <srudhi24@gmail.com>
2024-08-13 16:01:03 +0530

    updated changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import Image from "next/image";
-import { Main } from "@/components";
-import Section from "@/components/section";
+import styles from "./page.module.css";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
+import Details from "@/components/Details/Details";
+import Product from "@/components/Products/Products";
+
 export default function Home() {
   return (
     <main>
-      <Section />
-      <Main /> 
+      <Navbar />
+      <Details />
+      <Product />
+      <Footer />
     </main>
   );
 }
